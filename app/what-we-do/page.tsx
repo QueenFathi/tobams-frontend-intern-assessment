f7c6d6636e67016555edfe57d54043706c41656e
QueenFathi <fathiat477odutayo@gmail.com>
2026-04-24 16:13:06 +0100

    feat: clear home page and added necessary javascript packages and create what we do page

---
+++ app/what-we-do/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'What We Do | Tobams Group',
+  description: 'Our comprehensive range of programs and resources is designed to enhance skills, broaden knowledge, and propel careers forward in today’s ever-evolving landscape',
+}
+
+
+export default function About() {
+  return (
+    <div className="tracking-wider bg-background">
+      
+    </div>
+  );
+}
